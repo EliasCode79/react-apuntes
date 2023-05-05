0f367694363fd1550a0b18f07c8e7da1d95000e0
EliasCode79 <eliasfranco2379@gmail.com>
2023-05-05 08:10:15 -0400

    feat: :sparkles: impresion de variables en el HTML

--- 03-counter-vite-js/src/FirstComponent.jsx
+++ 03-counter-vite-js/src/FirstComponent.jsx
@@ -1,8 +1,22 @@
+const menu = {
+	title: 'menu',
+	msg: 'Hola desde menu',
+};
+
+const suma = (a, b) => {
+	return a + b;
+};
+
 export const FirtsApp = () => {
 	return (
 		<>
 			<div>Mi nombre es elias franco</div>
 			<p>hola</p>
+			{/* dentro de las llaves podemos imprimir expresiones y valores de js, pero no se pueden imprimir objetos */}
+			{menu.title /* dentro */}
+			<p>Llamando a un funcion: {suma(4, 5)}</p>
+			{/* Si queremos imprimir objetos podemos hacerlo de la sgte manera */}
+			<p>{JSON.stringify(menu)}</p>
 		</>
 	);
 };
