176de5c71d57c51730dd018b3271dc401adcad3b
EliasCode79 <eliasfranco2379@gmail.com>
2023-05-05 19:51:35 -0400

    feat: :sparkles: archivos css

--- 03-counter-vite-js/src/main.jsx
+++ 03-counter-vite-js/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { FirtsApp } from './FirstComponent';
 // import { App } from './HelloWorldApp';
+import './styles.css';
 
 ReactDOM.createRoot(document.querySelector('#root')).render(
 	<React.StrictMode>
