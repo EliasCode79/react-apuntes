81087710d8bc2d0361c2a5cf9dfce8ca17e88e48
EliasCode79 <eliasfranco2379@gmail.com>
2023-05-05 21:25:34 -0400

    feat: :sparkles: hooks useState counterApp

--- 03-counter-vite-js/src/CounterApp.jsx
+++ 03-counter-vite-js/src/CounterApp.jsx
@@ -1,16 +1,18 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 export const CounterApp = ({ value }) => {
+	const [counter, setCounter] = useState(value);
+
 	const handler = (event) => {
 		// console.log(event)
-		console.log('+1');
-		value = 100;
+		setCounter(counter + 1);
 	};
 
 	return (
 		<>
 			<h1>Counter App</h1>
-			<h2>{value}</h2>
+			<h2>{counter}</h2>
 
 			{/* onclick envia por defecto un event, en react podemos llamar a una funcion si (), cuando se envia un parametro */}
 			<button onClick={handler}>+1</button>
