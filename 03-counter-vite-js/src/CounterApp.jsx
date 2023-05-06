5086f78c03feb4894b3b64fbcf4db6609bb1dd06
EliasCode79 <eliasfranco2379@gmail.com>
2023-05-05 21:07:22 -0400

    feat: :sparkles: Eventos de forma nativa con JS

--- 03-counter-vite-js/src/CounterApp.jsx
+++ 03-counter-vite-js/src/CounterApp.jsx
@@ -1,10 +1,19 @@
 import PropTypes from 'prop-types';
 
 export const CounterApp = ({ value }) => {
+	const handler = (event) => {
+		// console.log(event)
+		console.log('+1');
+		value = 100;
+	};
+
 	return (
 		<>
 			<h1>Counter App</h1>
 			<h2>{value}</h2>
+
+			{/* onclick envia por defecto un event, en react podemos llamar a una funcion si (), cuando se envia un parametro */}
+			<button onClick={handler}>+1</button>
 		</>
 	);
 };
