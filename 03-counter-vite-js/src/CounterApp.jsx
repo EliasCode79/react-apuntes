8f2a39f1776828358f04d32f9fe7a45fb86ef725
EliasCode79 <eliasfranco2379@gmail.com>
2023-05-05 22:07:55 -0400

    feat: :sparkles: resta y reset con useState, alternativa#2

--- 03-counter-vite-js/src/CounterApp.jsx
+++ 03-counter-vite-js/src/CounterApp.jsx
@@ -5,18 +5,20 @@ export const CounterApp = ({ value }) => {
 	const [counter, setCounter] = useState(value);
 
 	const handler = (operation) => {
-		operation === 'add' ? setCounter(counter + 1) : operation === 'res' ? setCounter(counter - 1) : setCounter(0);
-		// setCounter(counter + 1);
+		setCounter(counter + 1);
 	};
+
+	const handlerRest = () => setCounter(counter - 1);
+	const handlerReset = () => setCounter(value);
 
 	return (
 		<>
 			<h1>Counter App</h1>
 			<h2>{counter}</h2>
 
-			<button onClick={() => handler('add')}>+1</button>
-			<button onClick={() => handler('res')}>-1</button>
-			<button onClick={() => handler('reset')}>Reset</button>
+			<button onClick={handler}>+1</button>
+			<button onClick={handlerRest}>-1</button>
+			<button onClick={handlerReset}>Reset</button>
 		</>
 	);
 };
