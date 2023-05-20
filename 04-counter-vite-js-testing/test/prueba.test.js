c26887517c4d4c13da5ab6ebeab56c2f3ab27f24
EliasCode79 <eliasfranco2379@gmail.com>
2023-05-20 12:47:31 -0400

    feat: se agrego @type/jest en devDenpendias para autocompletar los metodos de jest

--- 04-counter-vite-js-testing/test/prueba.test.js
+++ 04-counter-vite-js-testing/test/prueba.test.js
@@ -1,5 +1,9 @@
-test('esta prueba no puede fallar', () => {
-	if (0 === 3) {
-		throw new Error('son iguales, entrara aca y mostrara el error');
-	}
+describe('prueba de componenete <appComponete/>', () => {
+	test('esta prueba no puede fallar', () => {
+		let message = 'hola mundo';
+		let message2 = message.trim();
+
+		expect(message).toBe(message2);
+		expect(message).toBe(message2);
+	});
 });
