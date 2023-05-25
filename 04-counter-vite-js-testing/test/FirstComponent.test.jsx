a46ca046f8b2dd53a1bcc3a0f78f4fca853948b2
EliasCode79 <eliasfranco2379@gmail.com>
2023-05-24 23:54:44 -0400

    feat: snapshot, metodos toMatchSnapshot, esta test se asegura de q nuestro componente no vaya a cambiar de manera accidenteal el dia de mañana

--- 04-counter-vite-js-testing/test/FirstComponent.test.jsx
+++ 04-counter-vite-js-testing/test/FirstComponent.test.jsx
@@ -4,6 +4,11 @@ import { FirtsApp } from '../src/FirstComponent';
 describe('pruebas en FirstComponent', () => {
 	test('tiene q hacer match con el snapshott', () => {
 		const title = 'hola soy Elias';
-		render(<FirtsApp title={title} />);
+
+		// el metodo render, renderiza nuestro componente, tambien puede darnos muchos metodos para realizar diferentes operaciones con el dom. Como en el de abajo, nos devuelve un Node de html.
+		const { container } = render(<FirtsApp title={title} />);
+		// El sgte metodo se usa mayormente para un componente q no vaya a cambiar tanto.
+		// Con esta linea se creaa una archivo, con todos los elementos html(dom) del componente FirstsApp, la primera ves q se ejecutan este metodo se toma un snapshot de este componente y se almacena, la segunda vez q ejecutemos nos mostrara un error si se cambio algo del html(dom) y si no hubo cambios no mostrara nada. Si hicimos cambios en html(dom), podemos actualizar nuestro snapshot desde la terminal, hay nos indica como podemos hacerlo(normalmente presionando la letra 'u').
+		expect(container).toMatchSnapshot();
 	});
 });
