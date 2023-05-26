d4a02a75ec46570435e6e3320098ca1eeaf9b948
EliasCode79 <eliasfranco2379@gmail.com>
2023-05-26 11:30:31 -0400

    feat: nueva compenente <GifGrid />

--- 05-git-expert-app/src/GifExpertApp.jsx
+++ 05-git-expert-app/src/GifExpertApp.jsx
@@ -1,32 +1,24 @@
 import { useState } from 'react';
 import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
 
 export const GifExpertApp = () => {
 	const [categories, setCategories] = useState(['one punch', 'dragon ball']);
 
 	const onAddCategory = (newCategory) => {
 		if (categories.includes(newCategory)) return; // con esta linea hacemos q no haya valores repetidos y tambien resolvemos el tema del 'key' unico den los 'li'.
-		setCategories([...categories, newCategory]);
+		setCategories([newCategory, ...categories]);
 	};
 
 	return (
 		<>
-			{/* title */}
 			<h1>GifExpertApp</h1>
 
-			{/* input */}
-			<AddCategory
-				// setCategories={setCategories}
-				onNewValue={(event) => onAddCategory(event)}
-			/>
-			{/* grid table */}
+			<AddCategory onNewValue={(event) => onAddCategory(event)} />
 
-			<ol>
-				{/* Para recorrer un array usamo el map, a los elementos li tenemos q agregarles un key para q no nos presente problemas en la consola */}
-				{categories.map((categorie) => {
-					return <li key={categorie}>{categorie}</li>;
-				})}
-			</ol>
+			{categories.map((categorie) => (
+				<GifGrid key={categorie} category={categorie} />
+			))}
 		</>
 	);
 };
