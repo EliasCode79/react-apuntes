a93b6c09b90073698b60a78c114951421ff84e9f
EliasCode79 <eliasfranco2379@gmail.com>
2023-05-26 10:51:49 -0400

    feat: emitir un evento al padre

--- 05-git-expert-app/src/GifExpertApp.jsx
+++ 05-git-expert-app/src/GifExpertApp.jsx
@@ -4,9 +4,9 @@ import { AddCategory } from './components/AddCategory';
 export const GifExpertApp = () => {
 	const [categories, setCategories] = useState(['one punch', 'dragon ball']);
 
-	const onAddCategory = () => {
-		let nueva = 'demon slayer';
-		setCategories([...categories, nueva]);
+	const onAddCategory = (newCategory) => {
+		// console.log(newCategory);
+		setCategories([...categories, newCategory]);
 	};
 
 	return (
@@ -15,18 +15,18 @@ export const GifExpertApp = () => {
 			<h1>GifExpertApp</h1>
 
 			{/* input */}
-			<AddCategory setCategories={setCategories} />
+			<AddCategory
+				// setCategories={setCategories}
+				onNewValue={(event) => onAddCategory(event)}
+			/>
 			{/* grid table */}
 
-			{/* <button onClick={(e) => onAddCategory()}>Agregar</button> */}
 			<ol>
 				{/* Para recorrer un array usamo el map, a los elementos li tenemos q agregarles un key para q no nos presente problemas en la consola */}
 				{categories.map((categorie) => {
 					return <li key={categorie}>{categorie}</li>;
 				})}
 			</ol>
-
-			{/* grid item */}
 		</>
 	);
 };
--- 05-git-expert-app/src/components/AddCategory.jsx
+++ 05-git-expert-app/src/components/AddCategory.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-export const AddCategory = ({ setCategories }) => {
+export const AddCategory = ({ onNewValue }) => {
 	const [inputValue, setInputValue] = useState('init state');
 
 	// este metodod es para la entrada del input
@@ -13,13 +13,14 @@ export const AddCategory = ({ setCategories }) => {
 		event.preventDefault();
 		if (inputValue.trim().length <= 1) return; // Validacion de q por lo menos debe de haber 2 caracteres en la entrada del input.
 
-		setCategories((elementos) => [inputValue, ...elementos]);
+		// setCategories((elementos) => [inputValue, ...elementos]);
+		onNewValue(inputValue.trim());
 		setInputValue(''); // limpiamos nuestro valores.
 	};
 
 	return (
 		<form onSubmit={onSubmit}>
-			<input type='text' placeholder='introduce un texto' value={inputValue} onChange={onInputChange} />;
+			<input type='text' placeholder='introduce un texto' value={inputValue} onChange={onInputChange} />
 		</form>
 	);
 };
