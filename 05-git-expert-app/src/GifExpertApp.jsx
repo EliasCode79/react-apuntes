96a53207bc18ef2aa1ea6f6265dd8abeccd59ae5
EliasCode79 <eliasfranco2379@gmail.com>
2023-05-25 21:52:50 -0400

    feat: funcionalidada para agreagar una nueva categoria

--- 05-git-expert-app/src/GifExpertApp.jsx
+++ 05-git-expert-app/src/GifExpertApp.jsx
@@ -3,6 +3,16 @@ import { useState } from 'react';
 export const GifExpertApp = () => {
 	// podemos tener n cantidades de estados(useState), estos no tiene un nombre se identifican por el orden q se vayan creando. Asi q evita meter useState en condicionales porque afectara el orden de los useState.
 	const [categories, setCategories] = useState(['one punch', 'dragon ball']);
+
+	const onAddCategory = () => {
+		let nueva = 'demon slayer';
+		//  evitemos usar el push en los useState en react.
+		// TODO: tenemos q entender q useState, el estado no tiene de mutar(cambiar(agregar)), sino q tenemos q darle un nuevo estado.
+		setCategories([...categories, nueva]); // esta es otra forma.
+		// setCategories((cat) => [...categories, nueva]);  // esta es otra forma.
+		// setCategories(categories.concat(nueva));  // esta es un forma de hacerlo
+	};
+
 	return (
 		<>
 			{/* title */}
@@ -12,6 +22,7 @@ export const GifExpertApp = () => {
 
 			{/* grid table */}
 
+			<button onClick={(e) => onAddCategory()}>Agregar</button>
 			<ol>
 				{/* Para recorrer un array usamo el map, a los elementos li tenemos q agregarles un key para q no nos presente problemas en la consola */}
 				{categories.map((categorie) => {
