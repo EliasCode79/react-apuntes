9d1cb11e3c5d8c9c2f76927c06c872027ab786e2
EliasCode79 <eliasfranco2379@gmail.com>
2023-05-26 23:44:43 -0400

    feat: custom hook - useFetchGifs, hook personalizado

--- 05-git-expert-app/src/components/GifGrid.jsx
+++ 05-git-expert-app/src/components/GifGrid.jsx
@@ -1,18 +1,8 @@
-import { useEffect, useState } from 'react';
-import { getGifs } from '../helpers/getGifs';
 import { GitItem } from './GitItem';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 
 export const GifGrid = ({ category }) => {
-	const [images, setImages] = useState([]);
-
-	const getImages = async () => {
-		const newImages = await getGifs(category);
-		setImages(newImages);
-	};
-
-	useEffect(() => {
-		getImages();
-	}, []);
+	const { images, isLoading } = useFetchGifs(category);
 
 	return (
 		<>
