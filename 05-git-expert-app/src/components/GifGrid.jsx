0e543ff9959dbeec220e754193d349764a9b0cdb
EliasCode79 <eliasfranco2379@gmail.com>
2023-05-26 23:58:55 -0400

    feat: mostrar mensaje de carga #88

--- 05-git-expert-app/src/components/GifGrid.jsx
+++ 05-git-expert-app/src/components/GifGrid.jsx
@@ -7,6 +7,10 @@ export const GifGrid = ({ category }) => {
 	return (
 		<>
 			<h3>{category}</h3>
+
+			{/* Esta expresion si se cumplen las 2 retorna lo q es la segunda condicion */}
+			{isLoading && <h2>Cargando....</h2>}
+
 			<div className='card-grid'>
 				{images.map((image) => (
 					<GitItem key={image.id} {...image} />
